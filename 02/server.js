17018e8d537c2ea431ce9e06e986b4abbbd85021
敖晓强 <aoxiaoqiang@163.com>
2018-09-19 17:00:51 +0800

    feat: 注释

--- 02/server.js
+++ 02/server.js
@@ -4,6 +4,7 @@ const router = require('./router');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// 创建Server, 添加路由处理
 const server = http.createServer(router);
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
